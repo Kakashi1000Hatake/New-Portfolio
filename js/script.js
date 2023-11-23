81c9d15e441f828505aca1dd83bd1d5780963c96
Harsh Lakhanpal <harsh.lakhanpal3@gmail.com>
2023-11-23 23:28:14 +0530

    Projects Section

--- js/script.js
+++ js/script.js
@@ -30,3 +30,18 @@ changestylesheet.addEventListener("click", () => {
     stylesheet.setAttribute("href", "./css/style.css");
   }
 });
+
+// project slider
+
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+next.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").appendChild(items[0]);
+});
+
+prev.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").prepend(items[items.length - 1]);
+});
